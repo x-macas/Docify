0a339919581966d19d7244e20339c66f04b7e03d
Vishal Kumar Sharma <vksharma11235@gmail.com>
2025-06-01 17:29:16 +0530

    / after the url resolved

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ connectCloudinary()
 
 // middleware
 app.use(cors({ 
-  origin: ['https://docifyy.netlify.app/', 'https://docifyadmin.netlify.app/'], 
+  origin: ['http://localhost:5174','http://localhost:5173','https://docifyy.netlify.app', 'https://docifyadmin.netlify.app'], 
   credentials: true,
 }));
 app.use(express.json())
